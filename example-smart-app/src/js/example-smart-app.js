d68f1b8860f34e3d985e0b4ad00cf23c1852cc7a
firsthx <60949200+firsthx@users.noreply.github.com>
2020-10-19 12:15:56 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -60,7 +60,7 @@
           p.hdl = getQuantityValueAndUnit(hdl[0]);
           p.ldl = getQuantityValueAndUnit(ldl[0]);
           
-          p.zivnotes = JSON.stringify(obv);
+          p.zivnotes = "x" + JSON.stringify(obv);
 
           ret.resolve(p);
         });
